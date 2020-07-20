8b7ee0cc4989907a074c249f1a92a10c039e9ef6
mai cook <mailiecook@yahoo.com>
2020-07-20 13:54:20 -0700

    setup headlines.js

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -9,7 +9,7 @@ class Headlines extends React.Component {
       headlines: []
     };
   }
-makeApiCall = () => {
+  makeApiCall = () => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
       .then(response => response.json())
       .then(
@@ -19,14 +19,39 @@ makeApiCall = () => {
             headlines: jsonifiedResponse.results
           });
         })
-        .catch((error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+      .catch((error) => {
+        this.setState({
+          isLoaded: true,
+          error
         });
-  }
-  render() {
+      });
+    }
+    
+    componentDidMount() {
+      this.makeApiCall()
+    }
+  
+    render() {
+    const { error, isLoaded, headlines } = this.state;
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>;
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Headlines</h1>
+          <ul>
+            {headlines.map((headline, index) =>
+              <li key={index}>
+                <h3>{headline.title}</h3>
+                <p>{headline.abstract}</p>
+              </li>
+            )}
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
